01e0217107961b73da32de247e53c87bf18b147b
whereareiam <mmm.g032@gmail.com>
2023-11-30 10:34:45 +0100

    sync

---
+++ start.bat
@@ -0,0 +1,11 @@
+@echo off
+
+@rem check if java is installed
+WHERE java >nul 2>nul
+IF %ERRORLEVEL% NEQ 0 (
+  ECHO No valid java installation was found, please install java in order to run CloudNet
+  EXIT /b 1
+) ELSE (
+:: DO NOT CHANGE THE SUPPLIED MEMORY HERE. THIS HAS NO EFFECT ON THE NODE INSTANCE. USE THE launcher.cnl INSTEAD
+  java -Xms128M -Xmx128M -XX:+UseZGC -XX:+PerfDisableSharedMem -XX:+DisableExplicitGC -jar launcher.jar
+)
